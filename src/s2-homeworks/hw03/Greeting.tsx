69d8da582b5cd76d00e933cc50a4be02ac40ee39
Daniyel Tkachuk <danilaradyn7357899@gmail.com>
2024-05-22 21:52:33 +0300

    refactor HW-3

--- src/s2-homeworks/hw03/Greeting.tsx
+++ src/s2-homeworks/hw03/Greeting.tsx
@@ -25,7 +25,8 @@ const Greeting: React.FC<GreetingPropsType> = (
         lastUserName,
     } // деструктуризация пропсов
 ) => {
-    const inputClass = s.errorInput // need to fix with (?:)
+    // const inputClass = s.errorInput // need to fix with (?:)
+   const inputClass = s.input;
 
     return (
         <div id={'hw3-form'} className={s.greetingForm}>
@@ -57,7 +58,7 @@ const Greeting: React.FC<GreetingPropsType> = (
                     className={s.button}
                     disabled={!name.trim()}
                 >
-                    add
+                    Add
                 </button>
             </div>
 
